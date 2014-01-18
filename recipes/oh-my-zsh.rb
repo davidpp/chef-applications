60bec6a4aa69194e38c9b9027a40f2ca77c0584d
David Paquet Pitts <david.paquet@okam.ca>
2014-01-17 21:59:53 -0500

    Update oh-my-zsh.rb

--- recipes/oh-my-zsh.rb
+++ recipes/oh-my-zsh.rb
@@ -1,11 +1,6 @@
 case node["platform_family"]
     when 'mac_os_x'
-        include_recipe "applications::default"
-        #execute "oh my zsh install" do
-        command "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
-        user node['current_user']
-          #  not_if { File.exist?("/.oh-my-zsh") }
-       # end
+        include_recipe "applications::homebrewcask"
     when 'debian'
         Chef::Log.debug("This recipe is OSX only")
 end
